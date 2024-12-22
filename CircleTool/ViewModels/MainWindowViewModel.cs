8154ccfc7995699a5e98d790d99e02a5da443545
Martin Werner <m.werner.0@protonmail.com>
2024-12-22 03:34:42 +0100

    Implement temperature converter minimal example

--- CircleTool/ViewModels/MainWindowViewModel.cs
+++ CircleTool/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace CircleTool.ViewModels;
+﻿using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Diagnostics;
+
+namespace CircleTool.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    static private double CelciusToFahrenheit(double C) 
+        => C * 9.0 / 5.0 + 32.0;
+
+    [ObservableProperty]
+    private double _celcius;
+    partial void OnCelciusChanged(double value) 
+        => Fahrenheit = CelciusToFahrenheit(value);
+
+    [ObservableProperty]
+    private double _fahrenheit;
 }
