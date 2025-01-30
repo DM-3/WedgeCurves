3a83c83cf2d672ae277dff9f282cf3cda7e68e88
Martin Werner <m.werner.0@protonmail.com>
2025-01-30 17:51:32 +0100

    Implement solution to min approximation via dijkstra shortest path search (#3)

--- CircleTool/Logic/CircleApproximator.cs
+++ CircleTool/Logic/CircleApproximator.cs
@@ -61,43 +61,90 @@ public class CircleApproximator
 
     public static Point[] WedgesMin(double radius, bool oddCenter, bool narrow, bool outsideEdge)
     {
-        Console.WriteLine();
-        var fn_intersectsCircle = (Point from, Point to) => 
+        var fn_lineCost = (Point from, Point to) =>
         {
+            // return infinite cost if line intersects circle
             double m = (to.Y - from.Y) / (to.X - from.X);
             double n = to.Y - m * to.X;
-            double r = radius;
-
+            double r = radius - 0.02;       // prevent unreachable cells through small margin of error
             double radicand = r * r + r * r * m * m - n * n;
+            if (radicand > 0)
+                return double.PositiveInfinity;
 
-            return radicand >= 0;
+            // else return area between circle and line
+            double at = to.X * from.Y - from.X * to.Y;
+            double a1 = from.X == 0 ? 0.5 * Math.PI : Math.Atan(from.Y / from.X);
+            double a2 = Math.Atan(to.Y / to.X);
+            return at - radius * radius * (a1 - a2);
         };
 
+        // prepare graph nodes
         List<Point> cells = new List<Point>();
-
         double t = 0.5 * Convert.ToDouble(oddCenter);
-        double x = -t;
-        double y = Math.Ceiling(radius + t) - t;
-
-        if (oddCenter)
-        {
-            cells.Add(new Point(x, y));
-            cells.Add(new Point(y, x));
-
-            x++;
-        }
-
+        double x = t, y;
         do
         {
+            double accY = Math.Sin(Math.Acos(x / radius)) * radius;
+            y = Math.Ceiling(accY + t) - t;
             cells.Add(new Point(x, y));
-            cells.Add(new Point(y, x));
-
-            x++;
-            y = Math.Ceiling(Math.Tan(Math.Acos(x / radius)) * x + t) - t;
         }
-        while (x <= y);
+        while (++x < y);
 
-        return LineToWedges(cells, narrow, outsideEdge);
+        // prepare graph edge matrix
+        double[,] connectionMatrix = new double[cells.Count, cells.Count];
+        for (int i = 0; i < cells.Count; i++)
+            for (int j = i + 1; j < cells.Count; j++)
+                connectionMatrix[i, j] = fn_lineCost(cells[i], cells[j]);
+
+        // list for shortest path algorithm to work on
+        List<(int prev, double distance, bool unvisited)> list = new List<(int, double, bool)>(cells.Count);
+        for (int i = 0; i < cells.Count; i++)
+            list.Add((i, i == 0 ? 0 : double.PositiveInfinity, true));
+
+        // Dijkstra shortest path
+        while (list.Last().unvisited)
+        {
+            // move to unvisited cell with shortest path next
+            double minD = list.Where(e => e.unvisited).Select(e => e.distance).Min();
+            if (minD == double.PositiveInfinity)
+            {
+                Console.WriteLine($"Disconnected graph, endpoint unreachable for radius = {radius}");
+                return [];
+            }
+            int i = list.FindIndex(e => e.distance == minD);
+            
+            // mark cell as visited
+            list[i] = (list[i].prev, list[i].distance, false);      
+            
+            // update path costs for all neighbors of current cell
+            for (int j = i + 1; j < cells.Count; j++)
+            {
+                double newD = minD + connectionMatrix[i, j];
+                if (newD < list[j].distance)                        // check if path is better
+                    list[j] = (i, newD, list[j].unvisited);
+            }
+        }
+
+        // assemble points along path
+        List<Point> path = new List<Point>(){ cells.Last() };
+        int index = cells.Count - 1;
+        do
+        {
+            index = list[index].prev;
+            path.Add(cells[index]);
+        }
+        while (index != 0);
+
+        if (oddCenter)
+            path.Add(new Point(path.Last().X - 1, path.Last().Y));
+
+        // mirror path to bottom half
+        List<Point> path2 = new List<Point>();
+        foreach (var p in path)
+            path2.Add(new Point(p.Y, p.X));
+        path.AddRange(path2);
+
+        return LineToWedges(path, narrow,outsideEdge);
     }
 
     // expand line segments to wedges and blocks
